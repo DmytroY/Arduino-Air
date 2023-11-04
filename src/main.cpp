3d4cfde930eb4b33e070fe80885d277afc9eda0d
Dmytro Yakovenko <dmyto_yakovenko1@epam.com>
2023-11-05 00:23:29 +0200

    LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
 #include <Arduino.h>
+#include <UTFT.h>                         //connect Libriary for use TFT
+
+// main class constructors
+
+UTFT    myGLCD(CTE32_R2, 38, 39, 40, 41);   //Type of TFT
+
+extern uint8_t BigFont[];          // Declare which fonts we will be using
+extern uint8_t SmallFont[];        
+extern uint8_t SevenSegNumFont[];  
+int i = 0;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  Serial.println("Serial.begin(9600)");
+
+  // Initialisation of LCD
+  myGLCD.InitLCD(LANDSCAPE);              
+  myGLCD.clrScr();
+  myGLCD.setBackColor(0, 0, 0);    
+  myGLCD.setFont(SevenSegNumFont);
+  Serial.println("setup routine done");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  Serial.println("Led is ON");
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  Serial.println("Led is OFF");
-  delay(250);   
+  Serial.print("i = ");
+  Serial.println(i);
+
+  
+  myGLCD.clrScr();  
+  myGLCD.setColor((i%3)*122, ((i+1)%3)*122, ((i+2)%3)*122);
+  // myGLCD.setColor(255, 255, 255);
+  // myGLCD.fillRect(0, 0, 319, 13);
+
+  myGLCD.printNumI(i, CENTER, 70);
+  delay(1000);
+  i++;
 }
